4d601d037296ba4de5f75c39870cceb136c7a259
David Ford <dford@chatsoft.com>
2020-05-05 22:21:39 -0400

    initial commit

---
+++ MyTrader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyTrader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
